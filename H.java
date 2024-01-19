f31eb44f1d25c1929197350de7db7b4550a53b0e
Pratham Gupta <116946644+Pratham735505@users.noreply.github.com>
2024-01-19 23:04:44 +0530

    Add files via upload

---
+++ H.java
@@ -0,0 +1,20 @@
+import java.io.Console;
+import java.util.Scanner;
+
+public class H {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Prompt the user for input
+        System.out.print("Enter something: ");
+
+        // Read input from the user
+        //String userInput = scanner.nextLine();
+
+        int a= scanner.nextInt();
+        System.out.println(a);
+        
+        
+    }
+    
+}
---
+++ Hell.java
@@ -0,0 +1,11 @@
+public class Hell {
+    public static void main(String[] args) {
+        byte a;
+        int i;
+        i=258;
+        a=(byte)i;
+        System.out.println(a);
+        System.out.println("Hello, World!");
+    }
+}
+ 
